f94b2c90e6f5e0de1a3011c4b7054f049704364d
amatasa <amatasa@users.noreply.github.com>
2018-04-04 22:06:48 -0400

    Add files via upload
    Finished and tested login/signup. usertest.php has test objects

---
+++ website/usertest.php
@@ -0,0 +1,10 @@
+<?php
+
+	require 'user.php';
+
+	//$DB = new DB();
+	//$db = DB->connection();
+	//$user = User::withLogin("TestDummy", "dc647eb65e6711e155375218212b3964");
+	//$user = User::withSignup("alextest", "alex@gmail.com", "alex", "matasa", "testtest");
+
+?>
